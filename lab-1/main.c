8d1e81fd89818bf63333ddccb3e3e2f539045829
Anatoly Nadolsky <a.nadolsky@proton.me>
2024-02-15 19:45:43 +0300

    `feat:` implement basic MPI program

--- lab-1/main.c
+++ lab-1/main.c
@@ -2,5 +2,15 @@
 
 int main(int argc, char **argv)
 {
+    int size = 0;
+    int rank = 0;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    MPI_Finalize();
+
     return EXIT_SUCCESS;
 }
--- lab-1/main.h
+++ lab-1/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <mpi.h>
 #include <stdlib.h>
 
 int main(int argc, char **argv);
