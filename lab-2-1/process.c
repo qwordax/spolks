a060f5aee1b5798a5f74fd59093137549360a56c
Anatoly Nadolsky <a.nadolsky@proton.me>
2024-03-03 00:07:04 +0300

    `feat!:` implement collective operations

--- lab-2-1/process.c
+++ lab-2-1/process.c
@@ -29,6 +29,67 @@ int process(int argc, char **argv)
     MPI_Comm_size(comm, &gpsize);
     MPI_Comm_rank(comm, &gprank);
 
+    double start = MPI_Wtime();
+
+    int rows = args.nsize / gpsize + 1;
+
+    int from = gprank * rows;
+    int to = from + rows;
+
+    if (to > args.nsize) {
+        to = args.nsize;
+    }
+
+    int *a = (int *)malloc(args.nsize * args.nsize * sizeof(int));
+    int *b = (int *)malloc(args.nsize * args.nsize * sizeof(int));
+    int *c = (int *)malloc(args.nsize * args.nsize * sizeof(int));
+
+    int *counts = (int *)malloc(gpsize * sizeof(int));
+    int *displs = (int *)malloc(gpsize * sizeof(int));
+
+    if (gprank == 0) {
+        int tmp;
+
+        for (int i = 0; i < args.nsize * args.nsize; i++) {
+            getrandom(&tmp, sizeof(tmp), 0);
+            a[i] = tmp % 10;
+
+            getrandom(&tmp, sizeof(tmp), 0);
+            b[i] = tmp % 10;
+        }
+
+        for (int i = 0; i < gpsize; i++) {
+            int ifrom = i * rows;
+            int ito = ifrom + rows;
+
+            if (ito > args.nsize) {
+                ito = args.nsize;
+            }
+
+            counts[i] = (ito - ifrom) * args.nsize;
+            displs[i] = ifrom * args.nsize;
+        }
+    }
+
+    MPI_Scatterv(a, counts, displs, MPI_INT,
+        a, (to - from) * args.nsize, MPI_INT, 0, comm);
+
+    MPI_Bcast(b, args.nsize * args.nsize, MPI_INT, 0, comm);
+
+    compute(c, a, b, from, to);
+
+    MPI_Gatherv(c, (to - from) * args.nsize, MPI_INT,
+        c, counts, displs, MPI_INT, 0, comm);
+
+    MPI_Comm_free(&comm);
+
+    double end = MPI_Wtime();
+
+    if (gprank == 0) {
+        printf("[%d]\tgroup:\t%d\n", color, gpsize);
+        printf("\ttime:\t%.2fs\n", end - start);
+    }
+
     MPI_Finalize();
 
     return EXIT_SUCCESS;
