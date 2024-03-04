b472020f4d1ed510ed82dcefa32807ebb82de826
Anatoly Nadolsky <a.nadolsky@proton.me>
2024-03-04 17:09:41 +0300

    `feat:` update `compute` function

--- lab-2-1/compute.c
+++ lab-2-1/compute.c
@@ -1,15 +1,13 @@
 #include "compute.h"
 
-void compute(int *c, const int *a, const int *b, int from, int to)
+void compute(int *c, const int *a, const int *b, int rows, int cols)
 {
-    int n = args.nsize;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            c[i * cols + j] = 0;
 
-    for (int i = from; i < to; i++) {
-        for (int j = 0; j < n; j++) {
-            c[i * n + j] = 0;
-
-            for (int k = 0; k < n; k++) {
-                c[i * n + j] += a[i * n + k] * b[k * n + j];
+            for (int k = 0; k < cols; k++) {
+                c[i * cols + j] += a[i * cols + k] * b[k * cols + j];
             }
         }
     }
--- lab-2-1/compute.h
+++ lab-2-1/compute.h
@@ -1,8 +1,6 @@
 #ifndef COMPUTE_H
 #define COMPUTE_H
 
-#include "args.h"
-
-void compute(int *c, const int *a, const int *b, int from, int to);
+void compute(int *c, const int *a, const int *b, int rows, int cols);
 
 #endif
